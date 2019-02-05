5defd6efaba8b290fe4fd81fa3e1669bc386c7f2
abstract-animation-9914 <vm930@hotmail.com>
2019-02-05 18:05:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,26 +1,26 @@
-class Dog
-def initialize(name)
-  @name = name
-end 
+# class Dog
+# def initialize(name)
+#   @name = name
+# end 
 
-def initialize(breed)
-  @breed =breed
-end 
+# def initialize(breed)
+#   @breed =breed
+# end 
 
-def name
-  @name
-end 
+# def name
+#   @name
+# end 
 
-def breed
-  @breed
-end 
+# def breed
+#   @breed
+# end 
 
-def name=(new_name)
-    @name = new_name
-  end
+# def name=(new_name)
+#     @name = new_name
+#   end
 
-def breed=(new_breed)
-  @breed= new_breed
-end 
+# def breed=(new_breed)
+#   @breed= new_breed
+# end 
 
-end 
+# end 
