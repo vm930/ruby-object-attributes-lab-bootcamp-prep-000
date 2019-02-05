4f19a359f25209cb5c3b3ffe3706e68edec3be05
abstract-animation-9914 <vm930@hotmail.com>
2019-02-05 18:04:34 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+  def initialize(name)
+    @name = name
+  end 
+  
+  def name
+    @name
+  end 
+  
+  def initialize(job)
+    @job = job
+  end 
+  def job
+    @job
+  end 
+end 
